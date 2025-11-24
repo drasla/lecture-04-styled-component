5ccfbae192ea1ca04c0651c259d00ffc4c2f5a64
Tae.Garam <drasla@naver.com>
2025-11-24 09:27:41 +0900

    02. styled-component props 전달

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,25 @@ import styled from "styled-components";
 
 const Father = styled.div`
     display: flex;
-`
+`;
 
-const BoxOne = styled.div`
-    background-color: teal;
+const Box = styled.div`
+    background-color: ${(props) => props.bgColor};
     width: 100px;
     height: 100px;
-`
-
-const BoxTwo = styled.div`
-    background-color: tomato;
-    width: 100px;
-    height: 100px;
-`
+`;
 
 const Text = styled.span`
     color: white;
 `;
 
 function App() {
-
   return (
     <Father>
-        <BoxOne>
+        <Box bgColor={"teal"}>
             <Text>Hello World</Text>
-        </BoxOne>
-        <BoxTwo />
+        </Box>
+        <Box bgColor={"tomato"}/>
     </Father>
   )
 }
